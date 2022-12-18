e3ac9ae385a01c1964fd2a1c1d367fc1e87df4a0
NAYAN S. SONI <107099130+testgithubnayan@users.noreply.github.com>
2022-12-18 17:39:37 +0530

    Create Ride.java

---
+++ src/main/Ride.java
@@ -0,0 +1,53 @@
+package com.bridgelabz.test;
+
+public class Ride {
+    private int time;
+    private double distance;
+    private Category category;
+
+    public Ride(Category category, double distance, int time) {
+        this.distance = distance;
+        this.time = time;
+        this.category = category;
+    }
+    public enum Category{
+        NORMAL_RIDE (10, 1, 5),PREMIUM_RIDE(15,2,20);
+
+        public final double costPerKm;
+        public final int costPerMin;
+        public final double minFare;
+
+        Category(double costPerKm, int costPerMin, double minFare) {
+            this.costPerKm = costPerKm;
+            this.costPerMin = costPerMin;
+            this.minFare = minFare;
+        }
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
+
+
+
